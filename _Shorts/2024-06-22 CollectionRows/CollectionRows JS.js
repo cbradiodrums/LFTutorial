d810978a9384021462e9a6476940ed0e9c25d8d8
Christopher Burrows <99897839+cbradiodrums@users.noreply.github.com>
2024-06-22 19:13:55 -0400

    naming updates

--- _Shorts/2024-06-22 CollectionRows/CollectionRows JS.js
+++ _Shorts/2024-06-22 CollectionRows/CollectionRows JS.js
@@ -1,11 +1,11 @@
 // === Form Field Declarations ===
-const _COL = {fieldId: 1}
-const _DD = {fieldId: 2}
-const _ML = {fieldId: 3}
+const _approvalCOL = {fieldId: 1}
+const _clearanceDD = {fieldId: 2}
+const _denialReasonML = {fieldId: 3}
 
 // === Form Subscriptions ===
 forDenialReasons()
-const subscription = [_DD, _ML].forEach(field => LFForm.onFieldChange(
+const subscription = [_clearanceDD, _denialReasonML].forEach(field => LFForm.onFieldChange(
     event =>  forDenialReasons(event) , {fieldId: field['fieldId']}))
 
 // === Dynamic Form Functions ===
@@ -15,11 +15,11 @@ async function forDenialReasons (event) { console.log(event)
     if (!event) {
 
         // If the table is empty, we need the field populated to alter
-        let COLsize = LFForm.getFieldValues(_COL)
-        if (COLsize.length < 1) await LFForm.addSet(_COL)
+        let COLsize = LFForm.getFieldValues(_approvalCOL)
+        if (COLsize.length < 1) await LFForm.addSet(_approvalCOL)
 
-        await LFForm.hideFields(_ML)
-        await LFForm.deleteSet(_COL, 0) // IF set does not need rows to submit Form
+        await LFForm.hideFields(_denialReasonML)
+        await LFForm.deleteSet(_approvalCOL, 0) // IF set does not need rows to submit Form
 
         return; // exit the function and do not process further
     }
@@ -31,30 +31,30 @@ async function forDenialReasons (event) { console.log(event)
     // Switch cases for different field Subscriptions
     switch (fieldObj.fieldId) {
         // Determine to hide / show Denial Reason
-        case _DD['fieldId']: 
+        case _clearanceDD['fieldId']: 
             let status = LFForm.getFieldValues(fieldObj)
             switch (status) {
                 case "APPROVE":
-                    await LFForm.setFieldValues({fieldId: _ML['fieldId'], index: fieldObj['index']}, "")
-                    await LFForm.hideFields({fieldId: _ML['fieldId'], index: fieldObj['index']})
+                    await LFForm.setFieldValues({fieldId: _denialReasonML['fieldId'], index: fieldObj['index']}, "")
+                    await LFForm.hideFields({fieldId: _denialReasonML['fieldId'], index: fieldObj['index']})
                     break;
                 default: 
-                    await LFForm.showFields({fieldId: _ML['fieldId'], index: fieldObj['index']})
+                    await LFForm.showFields({fieldId: _denialReasonML['fieldId'], index: fieldObj['index']})
                     break;
             }
             break;
         // Determine whether to ask for a case #
-        case _ML['fieldId']:
-            let denialReason = LFForm.getFieldValues({fieldId: _ML['fieldId'], index: fieldObj['index']})
+        case _denialReasonML['fieldId']:
+            let denialReason = LFForm.getFieldValues({fieldId: _denialReasonML['fieldId'], index: fieldObj['index']})
             let arrest = /arrest/i.test(denialReason) 
 
             switch (arrest) {
                 case true:
-                    await LFForm.changeFieldSettings({fieldId: _ML['fieldId'], index: fieldObj['index']}, 
+                    await LFForm.changeFieldSettings({fieldId: _denialReasonML['fieldId'], index: fieldObj['index']}, 
                         {subtext: "Please Include Case #"})
                     break;
                 default:
-                    await LFForm.changeFieldSettings({fieldId: _ML['fieldId'], index: fieldObj['index']}, 
+                    await LFForm.changeFieldSettings({fieldId: _denialReasonML['fieldId'], index: fieldObj['index']}, 
                         {subtext: ""})
                     break;
             }

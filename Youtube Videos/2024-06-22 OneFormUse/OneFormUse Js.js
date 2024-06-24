06e728316481d0451bee858005aaa29243f56ced
Christopher Burrows <99897839+cbradiodrums@users.noreply.github.com>
2024-06-23 20:01:56 -0400

    Updates on latest Video

--- Youtube Videos/2024-06-22 OneFormUse/OneFormUse Js.js
+++ Youtube Videos/2024-06-22 OneFormUse/OneFormUse Js.js
@@ -7,19 +7,34 @@ const productionView = true // always keep fields in view
 const _PageMain = {fieldId: 1}
 const _PageInstructions = {fieldId: 2}
 const _SL = {fieldId: 3}
-const _approvalCOL = {fieldId: 4}
-const _clearanceDD = {fieldId: 5}
-const _denialReasonML = {fieldId: 6}
-const _EditButton = {fieldId: 7}
+// Approval Collection
+const _approvalCOL = {fieldId: 1}
+const _employeeName = {fieldId: 4}
+const _clearanceDD = {fieldId: 2}
+const _denialReasonML = {fieldId: 3}
+// Edit Button
+const _EditButton = {fieldId: 5}
+const a_currentStepNameHTML = {fieldId: 6}
 
 // === Form Load Functions
 onFormLoad()
 
 // === Form Subscriptions
 async function forSubscriptions (currentStepName) { if (testLogging) console.log("%c===FUNCTION CALLED(): forSubscriptions", "color: cyan", currentStepName)
+
+    switch (currentStepName) {
+        case "REVIEW":
+            await LFForm.onFieldChange(readOnlyForm, _EditButton)
+            break;
+        default:
+            await LFForm.hideFields(_EditButton, _clearanceDD, _denialReasonML)
+            break;
+    }
     forDenialReasons()
     const subscription = [_clearanceDD, _denialReasonML].forEach(field => LFForm.onFieldChange(
         event =>  forDenialReasons(event) , {fieldId: field['fieldId']}))
+
+    
 }
 
 // === Dynamic Form Field Functions
@@ -76,15 +91,38 @@ async function forDenialReasons (event) { console.log(event)
     }
 }
 
+function readOnlyForm () {  
+
+    // Exclusion Fields
+    let excludeFieldIds = [_EditButton['fieldId']]
+
+    // Fetch all fields that aren't read Only
+    let findFields = LFForm.findFields(
+        f => ( f.settings.readOnly !== true && !excludeFieldIds.includes(f.settings.fieldId) && !['Page', 'Form'].includes(f.settings.componentType) )
+    )
+    // console.log(findFields) // Uncomment to see console log!
+
+    let editPage = LFForm.getFieldValues(_EditButton)['value']
+    switch (editPage) {
+        case "YES": 
+            LFForm.enableFields(findFields)
+            break;
+        case "NO":
+        default:
+            LFForm.disableFields(findFields)
+            break;
+    }
+}
+
 
 // === Form Load Functions
-async function hideOnLoad () {  if (testLogging) console.log("===FunctionName: hideOnLoad() CALLED")
+async function hideOnLoad () {  if (testLogging) console.log("%c===FunctionName: hideOnLoad() CALLED", "color: red")
 
   let hiddenSwitches = []
   if (hiddenSwitches.length > 0) LFForm.hideFields(hiddenSwitches)
 }
 
-async function disableOnLoad () {  if (testLogging) console.log("===FunctionName: disableOnLoad() CALLED")
+async function disableOnLoad () {  if (testLogging) console.log("%c===FunctionName: disableOnLoad() CALLED", "color: red")
 
   // Trigger Switch to Disable the Correct Email Collection Fields
   let disableFields = [];
@@ -95,21 +133,24 @@ async function disableOnLoad () {  if (testLogging) console.log("===FunctionName
 async function onFormLoad () { 
 
     let currentStepName = ""
-    if (testLogging) currentStepName = ""
+    if (testLogging) currentStepName = "REVIEW"
 
     switch (currentStepName) {
         case "START":
+            if (testData) await testEnviroment(currentStepName)
             break;
         case "REVIEW":
+            if (testData) await testEnviroment(currentStepName)
             break;
         default:
             return;
     }
+    if (testLogging) console.log("%c===FunctionName: onFormLoad() CALLED", "color: red", currentStepName)
     forSubscriptions(currentStepName)
 }
 
 // === Utility Functions
-async function testEnviroment () { if (testLogging) console.log("===FunctionName: testEnviroment() CALLED")
+async function testEnviroment (currentStepName) { if (testLogging) console.log("===FunctionName: testEnviroment() CALLED", currentStepName)
 
   let shuffle = Math.floor(Math.random() * 2);
   if (testLogging) console.log(`Testing Scenario ${shuffle} called!`)
@@ -117,22 +158,29 @@ async function testEnviroment () { if (testLogging) console.log("===FunctionName
 
   switch (shuffle) {
     case 0:
-      employeeTest = { applicantName: ["Henry Bisquick", "Thomas DiGiorno III", "Beef Thompson", "Lovely Loverson", "Mr Nice Nice", "The Sublimer"], 
-      policeApproval: ["APPROVED", "APPROVED", "DENIED", "APPROVED", "APPROVED", "APPROVED"], 
-      denialReason: ["", "", "He Bad", "", "", ""]
+      employeeTest = { employeeName: ["Henry Bisquick", "Thomas DiGiorno III", "Beef Thompson", "Lovely Loverson", "Mr Nice Nice", "The Sublimer"], 
+      policeApproval: ["APPROVE", "APPROVE", "DENY", "APPROVE", "APPROVE", "APPROVE"], 
+      denialReason: ["", "", "Arrested", "", "", ""]
       }
       break;
     default:
-      employeeTest = { applicantName: ["Martha Washington Jr", "Haley OBoodle", "Seraphina Chequeormoneeorderr", "Lovely Loverson", "Mr Nice Nice", "The Sublimer"], 
-      policeApproval: ["APPROVED", "DENIED", "APPROVED", "APPROVED", "APPROVED", "APPROVED"], 
-      denialReason: ["", "Chitty Chitty bang Bang", "", "", "", ""]
+      employeeTest = { employeeName: ["Martha Washington Jr", "Haley OBoodle", "Seraphina Chequeormoneeorderr", "Lovely Loverson", "Mr Nice Nice", "The Sublimer"], 
+      policeApproval: ["APPROVE", "DENY", "APPROVE", "APPROVE", "APPROVE", "APPROVE"], 
+      denialReason: ["", "Needs more Information", "", "", "", ""]
       }
     }
 
-  await LFForm.addSet(_EmployeeCOL, employeeTest.applicantName.length)
-  // await LFForm.setFieldValues(_EmployeeDenialReason, employeeTest.denialReason)
-  // await LFForm.setFieldValues(_EmployeePoliceApproval, employeeTest.policeApproval)
-  await LFForm.setFieldValues(_EmployeeName, employeeTest.applicantName) 
+  await LFForm.addSet(_approvalCOL, employeeTest.employeeName.length)
+  switch (currentStepName) {
+    case "REVIEW":
+        await LFForm.setFieldValues(_clearanceDD, employeeTest.policeApproval)
+        await forDenialReasons() // Need something to reveal the denial forms.
+        await LFForm.setFieldValues(_denialReasonML, employeeTest.denialReason)
+    break;
+    default:
+        break;
+  }
+  await LFForm.setFieldValues(_employeeName, employeeTest.employeeName) 
   // disableOnLoad();
   return;
 }

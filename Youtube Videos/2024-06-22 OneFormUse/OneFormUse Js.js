91f9ae58e4f609cb9b7beb20fd472fef4c271183
Christopher Burrows <99897839+cbradiodrums@users.noreply.github.com>
2024-07-04 09:12:41 -0400

    Optimization for OneFormUse

--- Youtube Videos/2024-06-22 OneFormUse/OneFormUse Js.js
+++ Youtube Videos/2024-06-22 OneFormUse/OneFormUse Js.js
@@ -4,9 +4,6 @@ const testData = false // preload form with testData
 const productionView = true // always keep fields in view
 
 // === Form Field Declarations ===
-const _PageMain = {fieldId: 1}
-const _PageInstructions = {fieldId: 2}
-const _SL = {fieldId: 3}
 // Approval Collection
 const _approvalCOL = {fieldId: 1}
 const _employeeName = {fieldId: 4}

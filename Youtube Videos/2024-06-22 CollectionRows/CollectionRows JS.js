96d300078487331589b4bdfa78debfc3514a82e6
Christopher Burrows <99897839+cbradiodrums@users.noreply.github.com>
2024-11-14 07:08:36 -0500

    OneFormUse Overhaul! Collection Rows Update

--- Youtube Videos/2024-06-22 CollectionRows/CollectionRows JS.js
+++ Youtube Videos/2024-06-22 CollectionRows/CollectionRows JS.js
@@ -1,15 +1,20 @@
+// === Form Constants ===
+const freezeFunctions = [0]
+
 // === Form Field Declarations ===
 const _approvalCOL = {fieldId: 1}
-const _clearanceDD = {fieldId: 2}
-const _denialReasonML = {fieldId: 3}
+const _clearanceDD = {fieldId: 5}
+const _denialReasonML = {fieldId: 4}
 
 // === Form Subscriptions ===
 forDenialReasons()
 const subscription = [_clearanceDD, _denialReasonML].forEach(field => LFForm.onFieldChange(
-    event =>  forDenialReasons(event) , {fieldId: field['fieldId']}))
+    (event) => { if (!freezeFunctions[0]) forDenialReasons(event)}, {fieldId: field['fieldId']}))
 
 // === Dynamic Form Functions ===
-async function forDenialReasons (event) { console.log(event)
+async function forDenialReasons (event) { console.log("===Event Paramter:", event, freezeFunctions)
+
+    freezeFunctions.fill(1)
     
     // Adjust Table on Form Launch
     if (!event) {
@@ -21,12 +26,14 @@ async function forDenialReasons (event) { console.log(event)
         await LFForm.hideFields(_denialReasonML)
         await LFForm.deleteSet(_approvalCOL, 0) // IF set does not need rows to submit Form
 
+        freezeFunctions.fill(0)
+
         return; // exit the function and do not process further
     }
 
     // Retrieve the Options
     let fieldObj =  { fieldId: event.options[0]['fieldId'], index: event.options[0]['index']}
-    // console.log(event, fieldObj) // Test Logging ONLY!
+    console.log("---Event + fieldObj:", event, fieldObj) // Test Logging ONLY!
 
     // Switch cases for different field Subscriptions
     switch (fieldObj.fieldId) {
@@ -60,4 +67,20 @@ async function forDenialReasons (event) { console.log(event)
             }
             break;
     }
+
+    freezeFunctions.fill(0)
 }
+
+/* Explanation Process:
+Step 1: Define Fields and 
+    forDenialReason() ON LAUNCH 
+    (SKIP subscription)
+Step 2a: SHOW Subscription, Retrieve the
+     Options and show the dynamic 
+     show / hide use case
+Step 2b: Retrieve the Options and 
+    show how responsive the form is 
+    using case #.
+Step 3 (FINAL): Explain bad practices 
+    and callback hell. freezeFunctions
+*/
--- Youtube Videos/2024-06-22 OneFormUse/OneFormUse Js.js
+++ Youtube Videos/2024-06-22 OneFormUse/OneFormUse Js.js
@@ -1,30 +1,40 @@
 // === Environment Variables
-const testLogging = false // show console messages
-const testData = false // preload form with testData
-const productionView = true // access / view of fields switch
+const testStep = ["ASSIGN"] // Change preview to specific Form Step
+const testLogging = [false] // show console messages + ENABLE TEST STEP
+const testData = [false] // preload form with testData
+const productionView = [true] // access / view of fields switch
+
+// === Form Constants ===
+const freezeFunctions = [0]
+const logColor = {
+    onLoad: "orange",
+    subscription: "cyan",
+    lookup:"light green",
+    event: "pink",
+    utility: "yellow"
+}
 
 // === Form Field Declarations ===
 // Approval Collection
 const _approvalCOL = {fieldId: 1}
-const _employeeName = {fieldId: 4}
-const _clearanceDD = {fieldId: 2}
-const _denialReasonML = {fieldId: 3}
+const _employeeName = {fieldId: 2}
+const _clearanceDD = {fieldId: 5}
+const _denialReasonML = {fieldId: 4}
 // Edit Button
-const _EditButton = {fieldId: 5}
-const _currentStepNameHTML = {fieldId: 6}
+const _EditButton = {fieldId: 6}
 
 // === Form Load Functions
 onFormLoad()
 
 // === Form Subscriptions
-async function forSubscriptions (currentStepName) { if (testLogging) console.log("%c===FUNCTION CALLED(): forSubscriptions", "color: cyan", currentStepName)
+async function forSubscriptions (currentStepName) { if (testLogging[0]) console.log("%c===FUNCTION CALLED(): forSubscriptions", `color: ${logColor.subscription}`, currentStepName)
 
     switch (currentStepName) {
         case "REVIEW":
             await LFForm.onFieldChange(readOnlyForm, _EditButton)
         case "ASSIGN":
             const subscription = [_clearanceDD, _denialReasonML].forEach(field => LFForm.onFieldChange(
-                event =>  forDenialReasons(event) , {fieldId: field['fieldId']}))
+                (event) => {if (!freezeFunctions[0]) forDenialReasons(event) }, {fieldId: field['fieldId']}))
             break;
         default:
             return;
@@ -32,14 +42,28 @@ async function forSubscriptions (currentStepName) { if (testLogging) console.log
 }
 
 // === Dynamic Form Field Functions
-async function forDenialReasons (event) { if (testLogging) console.log("%c===FUNCTION CALLED(): forDenialReasons", "color: yellow", event)
+async function forDenialReasons (event) { if (testLogging[0]) console.log("%c===FUNCTION CALLED(): forDenialReasons", `color: ${logColor.event}`, event, freezeFunctions)
+    
+    if (!testData[0]) freezeFunctions.fill(1);
     
     // Adjust Table on Form Launch
-    if (!event) { if (testLogging) console.log("!!NO EVENT -- EXIT FUNCTION"); return }
+    if (!event) {
+
+        // If the table is empty, we need the field populated to alter
+        let COLsize = LFForm.getFieldValues(_approvalCOL)
+        if (COLsize.length < 1) await LFForm.addSet(_approvalCOL)
+
+        await LFForm.hideFields(_denialReasonML)
+        await LFForm.deleteSet(_approvalCOL, 0) // IF set does not need rows to submit Form
+
+        freezeFunctions.fill(0)
+
+        return; // exit the function and do not process further
+    }
 
     // Retrieve the Options
     let fieldObj =  { fieldId: event.options[0]['fieldId'], index: event.options[0]['index']}
-    // console.log(event, fieldObj) // Test Logging ONLY!
+    if (testLogging[0]) console.log("---Event + fieldObj:", event, fieldObj) // Test Logging ONLY!
 
     // Switch cases for different field Subscriptions
     switch (fieldObj.fieldId) {
@@ -49,10 +73,12 @@ async function forDenialReasons (event) { if (testLogging) console.log("%c===FUN
             switch (status) {
                 case "APPROVE":
                     await LFForm.setFieldValues({fieldId: _denialReasonML['fieldId'], index: fieldObj['index']}, "")
+                    await LFForm.changeFieldSettings({fieldId: _employeeName['fieldId'], index: fieldObj['index']}, {CSSClasses: ""})
                     await LFForm.hideFields({fieldId: _denialReasonML['fieldId'], index: fieldObj['index']})
                     break;
                 default: 
                     await LFForm.showFields({fieldId: _denialReasonML['fieldId'], index: fieldObj['index']})
+                    await LFForm.changeFieldSettings({fieldId: _employeeName['fieldId'], index: fieldObj['index']}, {CSSClasses: "denied"})
                     break;
             }
             break;
@@ -60,7 +86,7 @@ async function forDenialReasons (event) { if (testLogging) console.log("%c===FUN
         case _denialReasonML['fieldId']:
             let denialReason = LFForm.getFieldValues({fieldId: _denialReasonML['fieldId'], index: fieldObj['index']})
             let arrest = /arrest/i.test(denialReason) 
-
+            
             switch (arrest) {
                 case true:
                     await LFForm.changeFieldSettings({fieldId: _denialReasonML['fieldId'], index: fieldObj['index']}, 
@@ -73,9 +99,12 @@ async function forDenialReasons (event) { if (testLogging) console.log("%c===FUN
             }
             break;
     }
+
+    freezeFunctions.fill(0);
+    return;
 }
 
-function readOnlyForm () {  
+function readOnlyForm () { if (testLogging[0]) console.log("%c===FUNCTION CALLED(): readOnlyForm", `color: ${logColor.subscription}`)
 
     // Exclusion Fields
     let excludeFieldIds = [_EditButton['fieldId']]
@@ -83,8 +112,8 @@ function readOnlyForm () {
     // Fetch all fields that aren't read Only
     let findFields = LFForm.findFields(
         f => ( f.settings.readOnly !== true && !excludeFieldIds.includes(f.settings.fieldId) && !['Page', 'Form'].includes(f.settings.componentType) )
-    )
-    // console.log(findFields) // Uncomment to see console log!
+    ) 
+    if (testLogging[0]) console.log(findFields) // Uncomment to see console log!
 
     let editPage = LFForm.getFieldValues(_EditButton)['value']
     switch (editPage) {
@@ -100,12 +129,12 @@ function readOnlyForm () {
 
 
 // === Form Load Functions
-async function hideOnLoad (currentStepName) {  if (testLogging) console.log("%c===FunctionName: hideOnLoad() CALLED", "color: red", currentStepName)
+async function hideOnLoad (currentStepName) {  if (testLogging[0]) console.log("%c===FunctionName: hideOnLoad() CALLED", `color: ${logColor.onLoad}`, currentStepName)
 
-  let hiddenSwitches = [_currentStepNameHTML]
+  let hiddenSwitches = []
   if (!['REVIEW'].includes(currentStepName)) hiddenSwitches.push(_EditButton)
   if (!['ASSIGN', 'REVIEW'].includes(currentStepName)) hiddenSwitches.push(_clearanceDD, _denialReasonML)
-  if (testLogging) console.log(hiddenSwitches)
+  if (testLogging[0]) console.log(hiddenSwitches)
   if (hiddenSwitches.length > 0) {
     await LFForm.addSet(_approvalCOL, 1)
     await LFForm.hideFields(hiddenSwitches)
@@ -113,7 +142,7 @@ async function hideOnLoad (currentStepName) {  if (testLogging) console.log("%c=
   }
 }
 
-async function disableOnLoad (currentStepName) {  if (testLogging) console.log("%c===FunctionName: disableOnLoad() CALLED", "color: red", currentStepName)
+async function disableOnLoad (currentStepName) {  if (testLogging[0]) console.log("%c===FunctionName: disableOnLoad() CALLED", `color: ${logColor.onLoad}`, currentStepName)
 
   let disableFields = [];
   if (['REVIEW'].includes(currentStepName)) {
@@ -122,41 +151,41 @@ async function disableOnLoad (currentStepName) {  if (testLogging) console.log("
     await LFForm.disableFields(disableFields)
     await LFForm.deleteSet(_approvalCOL, LFForm.getFieldValues(_approvalCOL).length-1)
   }
-  if (testLogging) console.log(disableFields)
+  if (testLogging[0]) console.log(disableFields)
 }
 
 
 async function onFormLoad () { 
 
-    let currentStepName = "";
-    currentStepName = await LFForm.getFieldValues(_currentStepNameHTML)
-    if (testLogging) currentStepName = "ASSIGN"
+    if ((LFForm.isPreview && !testLogging[0]) || LFForm.isPrint) return; // Don't execute Code
+    let currentStepName = LFForm.step.name
+    if (testLogging[0]) currentStepName = testStep[0]
 
     switch (currentStepName) {
-        case "START":
-           await LFForm.changeFormSettings({title: "Start Employee Clearance Application"})
+        case "APPLICATION": // Employees are selected for clearance
+           await LFForm.changeFormSettings({title: "APPLICATION Employee Clearance Application"})
            break;
-        case "ASSIGN":
+        case "ASSIGN": // Employees are assigned Clearance
             await LFForm.changeFormSettings({title: "Assign Employee Clearance Approval"})
             break;
-        case "REVIEW":
+        case "REVIEW": // Supervisor approves
             await LFForm.changeFormSettings({title: "Review Employee Clearance Applications"})
             break;
         default:
             return;
     }
 
-    if (testLogging) console.log("%c===FunctionName: onFormLoad() CALLED", "color: red", currentStepName)
-    if (productionView) await disableOnLoad(currentStepName); await hideOnLoad(currentStepName);
+    if (testLogging[0]) console.log("%c===FunctionName: onFormLoad() CALLED", `color: ${logColor.onLoad}`, "Step:", currentStepName, "LFForm:", LFForm)
+    if (productionView[0]) await disableOnLoad(currentStepName); await hideOnLoad(currentStepName);
     await forSubscriptions(currentStepName)
-    if (testData) await testEnviroment(currentStepName)
+    if (testData) { await testEnviroment(currentStepName); }
 }
 
 // === Utility Functions
-async function testEnviroment (currentStepName) { if (testLogging) console.log("%c===FunctionName: testEnviroment() CALLED", "color: pink", currentStepName)
+async function testEnviroment (currentStepName) { if (testLogging[0]) console.log("%c===FunctionName: testEnviroment() CALLED", `color: ${logColor.onLoad}`, currentStepName)
 
   let shuffle = Math.floor(Math.random() * 2);
-  if (testLogging) console.log(`Testing Scenario ${shuffle} called!`)
+  if (testLogging[0]) console.log(`Testing Scenario ${shuffle} called!`)
   let employeeTest;
 
   switch (shuffle) {
@@ -190,5 +219,6 @@ async function testEnviroment (currentStepName) { if (testLogging) console.log("
         break;
   }
   await LFForm.setFieldValues(_employeeName, employeeTest.employeeName) 
+  testData.fill(false) // Test data Entered!
   return;
 }
